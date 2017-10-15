1b8521db341e4791bb348e174681757bd066c662
Bryan Kruman <bryankruman@gmail.com>
2017-10-14 23:27:41 -0400

    Begin creating an application which uses voice recognition to enact commands/perform automated functions

---
+++ Remator/Remator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Windows.Forms;
+
+namespace Remator
+{
+	public partial class Remator : Form
+	{
+		public Remator()
+		{
+			InitializeComponent();
+		}
+
+		static SpeechRecognitionEngine engine;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			engine = new SpeechRecognitionEngine();
+			engine.SetInputToDefaultAudioDevice();
+			var words = new String[] { "shutdown my computer", "turn up the lights", "mator", "turn down the lights", "what's the weather like?" };
+			Choices choices = new Choices(words);
+			GrammarBuilder gb = new GrammarBuilder(choices);
+			Grammar g = new Grammar(gb);
+			engine.LoadGrammar(g);
+			engine.RecognizeAsync(RecognizeMode.Multiple);
+			engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
+		}
+
+		static void engine_SpeechRecognized(object ob, SpeechRecognizedEventArgs e)
+		{
+			//e.Result.
+			Console.WriteLine(string.Format("{0} : {1}", e.Result.Confidence, e.Result.Text));
+		}
+	}
+}
