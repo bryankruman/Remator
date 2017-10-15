e8850fde22a2726c97a275a700a5c6bd59cbbb83
Bryan Kruman <bryankruman@gmail.com>
2017-10-15 16:23:18 -0400

    Start using a command system that makes it easier to associate command names with methods

--- Remator/Remator.cs
+++ Remator/Remator.cs
@@ -1,42 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Speech;
+using System.Reflection;
 using System.Speech.Recognition;
+using System.Speech.Synthesis;
 using System.Windows.Forms;
 
 namespace Remator
 {
 	public partial class Remator : Form
 	{
+		private static Dictionary<string, MethodInfo> commandDictionary = new Dictionary<string, MethodInfo>();
+
+		public static Dictionary<string, MethodInfo> GetCommands()
+		{
+			if (commandDictionary.Count == 0)
+			{
+				var methods = typeof(Commands).GetMethods(
+					BindingFlags.Public | BindingFlags.DeclaredOnly | BindingFlags.Instance
+				);
+
+				var speechAttributeMethods = methods.Where(
+					y => y.GetCustomAttributes().OfType<CommandAttribute>().Any()
+				);
+
+				foreach (var speechAttributeMethod in speechAttributeMethods)
+				{
+					foreach (var attribute in speechAttributeMethod.GetCustomAttributes(true))
+					{
+						commandDictionary.Add(
+							((CommandAttribute)attribute).CommandValue, speechAttributeMethod
+						);
+					}
+				}
+			}
+
+			return commandDictionary;
+		}
+
+		public static MethodInfo GetMethod(string Command)
+		{
+			return GetCommands()[Command];
+		}
+
 		public Remator()
 		{
 			InitializeComponent();
 		}
 
 		static SpeechRecognitionEngine engine;
-		private void Form1_Load(object sender, EventArgs e)
+		//static SpeechSynthesizer synth ;
+		private void Remator_Load(object sender, EventArgs e)
 		{
 			engine = new SpeechRecognitionEngine();
 			engine.SetInputToDefaultAudioDevice();
-			var words = new String[] { "shutdown my computer", "turn up the lights", "mator", "turn down the lights", "what's the weather like?" };
-			Choices choices = new Choices(words);
-			GrammarBuilder gb = new GrammarBuilder(choices);
-			Grammar g = new Grammar(gb);
-			engine.LoadGrammar(g);
-			engine.RecognizeAsync(RecognizeMode.Multiple);
 			engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
+
+			Choices choices = new Choices(GetCommands().Keys.ToArray());
+			Grammar grammar = new Grammar(new GrammarBuilder(choices));
+			engine.LoadGrammar(grammar);
+
+			engine.RecognizeAsync(RecognizeMode.Multiple);
 		}
 
-		static void engine_SpeechRecognized(object ob, SpeechRecognizedEventArgs e)
+		static void engine_SpeechRecognized(object ob, SpeechRecognizedEventArgs args)
 		{
-			//e.Result.
-			Console.WriteLine(string.Format("{0} : {1}", e.Result.Confidence, e.Result.Text));
+			GetMethod(args.Result.Text).Invoke(Commands.list, null);
+		}
+
+		public static void synth_SynthesizeSpeech(string speechText)
+		{
+			var synth = new SpeechSynthesizer();
+			//synthesizer.SelectVoice()
+			synth.SetOutputToDefaultAudioDevice();
+			synth.SpeakAsync(speechText);
+			//synth.Dispose();
+			//synth = null;
+		}
+	}
+
+	public class Commands
+	{
+		public static Commands list = new Commands();
+
+		[Command("mator what's the weather like")]
+		[Command("mator what temperature is it outside")]
+		[Command("mator what's the temperature outside")]
+		[Command("mator what's the outside temperature")]
+		public void GetWeather()
+		{
+			Remator.synth_SynthesizeSpeech("It's currently 45 degrees outside");
+		}
+
+		[Command("mator what time is it")]
+		[Command("mator what's the time")]
+		public void GetTime()
+		{
+			Remator.synth_SynthesizeSpeech(DateTime.Now.ToLocalTime().ToShortTimeString());
+			Console.WriteLine(DateTime.Now.ToLocalTime().ToShortTimeString());
+		}
+
+		[Command("mator")]
+		[Command("mater")]
+		public void Mator()
+		{
+			Remator.synth_SynthesizeSpeech("I didn't catch that, try again?");
+			//Console.WriteLine("mator!!!!!");
+		}
+	}
+
+	[System.AttributeUsage(System.AttributeTargets.Method, AllowMultiple = true)]
+	public class CommandAttribute : System.Attribute
+	{
+		public string CommandValue { get; set; }
+
+		public CommandAttribute(string textValue)
+		{
+			CommandValue = textValue;
 		}
 	}
 }
